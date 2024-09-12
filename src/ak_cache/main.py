8edd597567c4f62bfdd3a46d01e8a99a373cae5d
Arun Kishore <50844460+rpakishore@users.noreply.github.com>
2024-09-12 13:34:28 -0700

    PEP8

--- src/ak_cache/main.py
+++ src/ak_cache/main.py
@@ -1,23 +1,26 @@
-from pathlib import Path
-import ak_file
-import pickle
 import base64
+import pickle
+from pathlib import Path
+from typing import Any
+
+import ak_file
 from cryptography.fernet import Fernet
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
-from typing import Any
+
 
 class Cache:
-    def __init__(self, filepath: Path= Path("Cache.pkl"), 
-                    password: str|None = None) -> None:
+    def __init__(
+        self, filepath: Path = Path("Cache.pkl"), password: str | None = None
+    ) -> None:
         self.filepath: Path = Path(str(filepath))
         self.file: ak_file.File = ak_file.File(str(filepath))
         self._create_cache_file(overwrite_existing=False)
         if password:
-            self.f = _generate_key(password = password)
+            self.f = _generate_key(password=password)
         else:
             self.f = None
-            
+
         self._cache_content: Any = None
 
     def __str__(self) -> str:
@@ -25,29 +28,29 @@ class Cache:
 
     def __repr__(self) -> str:
         return f"Cache(filepath={self.filepath})"
-    
+
     def _create_cache_file(self, overwrite_existing: bool = False) -> Path:
         if (not self.file.exists()) or overwrite_existing:
-            with open(self.filepath, 'wb'):
+            with open(self.filepath, "wb"):
                 pass
         return self.filepath
-    
+
     def write(self, data: Any) -> Path:
-        self._create_cache_file(overwrite_existing = False)
+        self._create_cache_file(overwrite_existing=False)
         byte_data: bytes = pickle.dumps(data)
         if self.f:
             byte_data = self.f.encrypt(byte_data)
-        _write_bytes_to_file(data=byte_data, filepath = self.filepath)
-        self._cache_content:Any = data
+        _write_bytes_to_file(data=byte_data, filepath=self.filepath)
+        self._cache_content: Any = data
         return self.filepath
-    
+
     @property
     def value(self):
         if self._cache_content is None:
             self._cache_content: Any = self.read()
-        
+
         return self._cache_content
-    
+
     def read(self) -> Any:
         byte_data: bytes = _read_bytes_from_file(self.filepath)
         if self.f:
@@ -56,27 +59,31 @@ class Cache:
             return pickle.loads(byte_data)
         except pickle.UnpicklingError:
             raise Exception(
-                "Cannot Unpickle. Are you sure this is not an encrypted cache file?")
+                "Cannot Unpickle. Are you sure this is not an encrypted cache file?"
+            )
+
 
 def _write_bytes_to_file(data: bytes, filepath: Path) -> None:
     with open(filepath, "wb") as f:
         f.write(data)
 
+
 def _read_bytes_from_file(filepath: Path) -> bytes:
     with open(filepath, "rb") as f:
         return f.read()
+
 
 def _generate_key(password: str) -> Fernet:
     """
     Generates a key from the given password and returns it.
     """
-    password_bytes = bytes(password, 'utf-8')
-    salt = b'salt_^h.W#(e6-OHplcig:?6@+((8{_f2skE'
+    password_bytes = bytes(password, "utf-8")
+    salt = b"salt_^h.W#(e6-OHplcig:?6@+((8{_f2skE"
     kdf = PBKDF2HMAC(
         algorithm=hashes.SHA256(),
         length=32,
         salt=salt,
         iterations=390000,
-        )
+    )
     key = base64.urlsafe_b64encode(kdf.derive(password_bytes))
     return Fernet(key)
--- src/tests/test_main.py
+++ src/tests/test_main.py
@@ -1,21 +1,27 @@
-from ak_cache.main import Cache
 from pathlib import Path
-import pytest
 from tempfile import TemporaryDirectory
+
+import pytest
+
+from ak_cache.main import Cache
+
 
 @pytest.fixture
 def cache_file(tmpdir):
-    return Path(tmpdir) / 'cache.pkl'
+    return Path(tmpdir) / "cache.pkl"
+
 
 @pytest.fixture
 def cache(cache_file):
     return Cache(cache_file)
 
+
 @pytest.fixture
 def encrypted_cache(tmpdir):
-    password = 'password'
-    cache_file = Path(tmpdir) / 'cache.pkl'
+    password = "password"
+    cache_file = Path(tmpdir) / "cache.pkl"
     return Cache(cache_file, password)
+
 
 def test_create_cache_file(cache_file):
     assert not cache_file.exists()
@@ -24,24 +30,28 @@ def test_create_cache_file(cache_file):
     cache._create_cache_file(overwrite_existing=True)
     assert cache_file.exists()
 
+
 def test_write_and_read(cache):
-    data = {'key': 'value'}
+    data = {"key": "value"}
     cache.write(data)
     assert cache.read() == data
 
+
 def test_write_and_read_encrypted(encrypted_cache):
-    data = {'key': 'value'}
+    data = {"key": "value"}
     encrypted_cache.write(data)
     assert encrypted_cache.read() == data
+
 
 def test_read_nonexistent_cache(cache_file):
     with pytest.raises(Exception):
         cache = Cache(cache_file)
         cache.read()
 
+
 def test_read_corrupt_cache(cache_file):
-    with open(cache_file, 'w') as f:
-        f.write('corrupt data')
+    with open(cache_file, "w") as f:
+        f.write("corrupt data")
     cache = Cache(cache_file)
     with pytest.raises(Exception):
         cache.read()
